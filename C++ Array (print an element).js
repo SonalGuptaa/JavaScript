fd34ab898cd5f3c7462be0b96f8ef1474cc981e3
sonalgupta01 <94384639+SonalGuptaa@users.noreply.github.com>
2024-03-18 00:18:21 +0530

    Add files via upload

---
+++ C++ Array (print an element).js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+    * @param number n
+    * @param number key
+    * @param number[] arr
+    
+    * @returns number
+    */
+        findElementAtIndex(n, key, arr) {
+            
+            let temp = arr[key];
+            return temp;
+        }
+    }
