c7cb0a49c5913c7b6152468dfd1ac046538f08af
sonalgupta01 <94384639+SonalGuptaa@users.noreply.github.com>
2024-03-20 11:19:26 +0530

    Findindex

---
+++ FindIndex.js
@@ -0,0 +1,31 @@
+class Solution {
+    findIndex(arr,n,key){
+       //code here
+       let ans = [];
+       
+    //   let first;
+       for(let i=0;i<n;i++)
+       {
+           if(arr[i] == key)
+           {
+               ans.push(i);
+               break;
+           }
+       }
+       
+       for(let i=n;i>=0;i--)
+       {
+           if(arr[i] == key)
+           {
+               ans.push(i);
+               break;
+           }
+           
+       }
+       if(ans.length == 0)
+       {
+           ans.push(-1,-1)
+       }
+       return ans;
+    }
+}
