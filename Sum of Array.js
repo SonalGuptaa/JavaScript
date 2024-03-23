6186f00133dbd71d7c6524a403a3837bd3f8fed2
sonalgupta01 <94384639+SonalGuptaa@users.noreply.github.com>
2024-03-24 00:08:18 +0530

    Day4

--- Sum of Array.js
+++ Sum of Array.js
@@ -1,11 +1,11 @@
 class Solution {
     sum(arr,n) {
         // code here
-        let sumi =0;
+        let sum =0 ;
         for(let i=0;i<n;i++)
         {
-            sumi+=arr[i];
+            sum+= arr[i];
         }
-        return sumi;
+        return sum;
     }
 }
