5c84dfb65a573d1a3dd4ed11de58c7ac81d7909e
sonalgupta01 <94384639+SonalGuptaa@users.noreply.github.com>
2024-03-28 06:26:46 +0530

    Day 8

---
+++ Swap kth elements.js
@@ -0,0 +1,12 @@
+swapKth(n, k, arr) {
+    // code here
+    
+    let t1 = arr[k-1];
+    let t2 = arr[n-k];
+    
+    // Change the value of arr[2] and arr[7]\
+    
+    arr[k-1] = t2;
+    arr[n-k] = t1;
+}
+}
